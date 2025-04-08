69159d45b60b43eabd7c73c66af7e6d727375af2
IqbalAbhipraya <abhipraya3105@gmail.com>
2025-04-08 17:03:41 +0700

    update soal3

--- soal3.js
+++ soal3.js
@@ -1,31 +1,17 @@
-/* 
-Abah telah menyiapkan sebuah kue ulang tahun besar yang diletakkan  di meja ruang jamu istana. 
-Namun, saat abah akan memotong kue tersebut, Abah  menyadari bahwa kue itu hilang!
+function searchThief(guestList, haveCakePhoto) {
+    if (!guestList.includes(haveCakePhoto)) {
+        return "Tidak bisa dicari pelakunya";
+    }
 
-- Ningguang selalu memeriksa kue sebelum memberikan kado.
-- Hutao langsung memberikan kado tanpa memperhatikan kue. 
-- Xiao memiliki kebiasaan memotret apa pun yang dia lihat pertama kali di  ruangan. 
-- Childe selalu membawa air mineral dan meletakkannya di meja sebelum  memberikan kado.
+    let suspect = guestList.slice(guestList.indexOf(haveCakePhoto)+1);
+    
+    if (suspect.length === 1){
+        return "Yang paling mungkin mengambil kue adalah " + suspect[0];
+    }else {
+        return "Tidak bisa dicari pelaku yang paling mungkin";
+    }
+}
 
-Urutan Masuk ke ruang jamu istana:
-- Ningguang
-- Hutao
-- Xiao
-- Childe
+guest = ["Ningguang", "Hutao", "Xiao", "Childe"];
 
-Yanfei meminta foto ke Xiao untuk menunjukkan foto yang diambil saat dia masuk ruang jamu istana. 
-Foto tersebut  menunjukkan kue masih utuh di meja.  
-
-Clue 1 = Foto Xiao = kue masih utuh, berarti Ningguang dan Hutao tidak mungkin mengambil kue
-
-Tersangka = Xiao (karena xiao hanya memotret yg dia lihat pertama kali) dan Childe
-
-Clue 2 = kue ulang tahun besar = Childe meletakkan air mineral di meja, 
-berarti tidak ada kue di meja saat Childe masuk
-
-Tersangka = Xiao
-
-Yang paling mungkin mengambil kue adalah Xiao
-*/
-
-console.log("Xiao");
+console.log(searchThief(guest, "Xiao"));
